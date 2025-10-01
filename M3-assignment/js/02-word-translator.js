fbe937ee3df4a5b9feb1dbe1fc17abd629631cda
Kaitlyn Gonzalez <kaitlyn.l.gonzalez@gmail.com>
2025-09-30 21:27:34 -0700

    Word Translator Revised

--- M3-assignment/js/02-word-translator.js
+++ M3-assignment/js/02-word-translator.js
@@ -1,13 +1,13 @@
 // prompt for language code ("es", "de", "en", and "fr"). If enter one of four codes, display translated version of "Hello World" in given language in console. If enters else, default English.
 let welcomeLanguage = prompt('Enter one of the following codes: es / de / en / fr')
 if (welcomeLanguage === 'es') {
-    console.log('Hola Mundo')
+    console.log('Hello World Translated in Spanish is: Hola Mundo')
 }  else if (welcomeLanguage === 'de') {
-    console.log('Hallo Welt')
+    console.log('Hello World Translated in Deutsch is: Hallo Welt')
 } else if (welcomeLanguage === 'en') {
-    console.log('Hello World')
+    console.log('Hello World translated in English is: Hello World')
 } else if(welcomeLanguage === 'fr') {
-    console.log('Bonjour le monde')
+    console.log('Hello World translated in French is: Bonjour le monde')
 } else {
-    console.log('Hello World')
+    console.log('Hello World translated in English is: Hello World')
 }
